58536a6cbf86af9a1ba434464159bf2dd37c0898
jeorb <andy.meier@gmail.com>
2018-08-14 22:21:15 -0500

    Filled in the rest of the operators.

--- src/main.rs
+++ src/main.rs
@@ -41,14 +41,15 @@ fn main() -> io::Result<()> {
     
     println!("First platter {} {:032b} {} {}", arrays[0][0], arrays[0][0], arrays.len(), arrays[0].len());
 
+    let mut cycles = 0;
     let mut finger = 0;
     while finger < arrays[0].len() {
         let platter = arrays[0][finger];
         let operator = platter >> 28;
-        let a = (platter & 7) as usize;
-        let b = (platter >> 3 & 7) as usize;
-        let c = (platter >> 6 & 7) as usize;
-        println!("Operator {} {:04b} - {} {} {}", operator, operator, a, b, c);
+        let a = (platter & 0b111) as usize;
+        let b = (platter >> 3 & 0b111) as usize;
+        let c = (platter >> 6 & 0b111) as usize;
+        println!("{} Operator {} {:04b} - {} {} {} {:032b}", finger, operator, operator, a, b, c, platter);
         match operator {
             0 => {
                 println!("Conditional Move");
@@ -58,50 +59,74 @@ fn main() -> io::Result<()> {
             },
             1 => {
                 println!("Array Index");
+                registers[a] = arrays[registers[b] as usize][registers[c] as usize];
             },
             2 => {
                 println!("Array Amendment");
+                arrays[registers[a] as usize][registers[b] as usize] = registers[c];
             },
             3 => {
                 println!("Addition");
+                registers[a] = registers[b] + registers[c];
             },
             4 => {
                 println!("Multiplication");
+                registers[a] = registers[b] * registers[c];
             },
             5 => {
                 println!("Division");
+                registers[a] = registers[b] / registers[c];
             },
             6 => {
                 println!("Not-And");
+                registers[a] = !(registers[b] | !registers[c]);
             },
             7 => {
                 println!("Halt");
+                break;
             },
             8 => {
                 println!("Allocation");
+                arrays.push(vec![0, registers[c]]);
+                registers[a] = (arrays.len()-1) as u32;
             },
             9 => {
                 println!("Abandonment");
+                arrays[registers[c] as usize] = vec![0];
             },
             10 => {
                 println!("Output");
+                println!("-------------------- {} - {}", registers[c] as u8 as char, registers[c]);
             },
             11 => {
                 println!("Input");
+                let input: u8 = io::stdin().bytes().next().and_then(|result| result.ok()).unwrap();
+                if input == '\n' as u8 {
+                    registers[c] = !0;
+                } else {
+                  registers[c] = input as u32;
+                }
             },
             12 => {
                 println!("Load Program");
+                arrays[0] = arrays[registers[b] as usize].to_vec();
+                finger = registers[c] as usize - 1;
             },
             13 => {
                 println!("Orthography");
+                let a = ((platter >> 25) & 0b111) as usize;
+                registers[a] = platter & 0b11111111_11111111_11111111_1;
+                println!("Orthography {} {}", a, registers[a]);
             },
             _ => {
                 println!("Whoops!!");
+                break;
             }
             
         }
         finger+=1;
-        if finger  > 10 {
+        cycles+=1;
+        if finger  > 20 || cycles > 20 {
             break;
         }
     }
